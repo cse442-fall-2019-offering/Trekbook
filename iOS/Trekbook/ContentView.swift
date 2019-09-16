b6d8b4204958df6239df461bfd49c4826f62a0e9
Dennis Fedorishin <dcfedori@buffalo.edu>
2019-09-16 13:31:34 -0400

    Initial project setup for Trekbook mobile

---
+++ iOS/Trekbook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trekbook
+//
+//  Created by Dennis Fedorishin on 9/16/19.
+//  Copyright © 2019 Trekbook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
