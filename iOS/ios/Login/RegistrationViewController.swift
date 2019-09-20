649a05f1201d5f4fb3666446e47108ae84772181
Dennis Fedorishin <dcfedori@buffalo.edu>
2019-09-19 20:49:50 -0700

    TB-19: Added buttons for signin and registration

---
+++ iOS/ios/Login/RegistrationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RegistrationViewController.swift
+//  iOS
+//
+//  Created by Dennis Fedorishin on 9/19/19.
+//  Copyright © 2019 Trekbook. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+    
+}
---
+++ iOS/ios/MapView/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  iOS
+//
+//  Created by Dennis Fedorishin on 9/19/19.
+//  Copyright © 2019 Trekbook. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
