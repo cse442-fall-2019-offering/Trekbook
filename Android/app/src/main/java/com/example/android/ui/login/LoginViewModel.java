f392ac1ff0d08a6cbad2e0d606ed095615e626f4
Liam Orr <liamorr@buffalo.edu>
2019-09-22 17:15:32 -0400

    TB-13: removed all parsing functionality textfields

--- Android/app/src/main/java/com/example/android/ui/login/LoginViewModel.java
+++ Android/app/src/main/java/com/example/android/ui/login/LoginViewModel.java
@@ -52,18 +52,20 @@ public class LoginViewModel extends ViewModel {
 
     // A placeholder username validation check
     private boolean isUserNameValid(String username) {
-        if (username == null) {
-            return false;
-        }
-        if (username.contains("@")) {
-            return Patterns.EMAIL_ADDRESS.matcher(username).matches();
-        } else {
-            return !username.trim().isEmpty();
-        }
+        return true;
+//        if (username == null) {
+//            return false;
+//        }
+//        if (username.contains("@")) {
+//            return Patterns.EMAIL_ADDRESS.matcher(username).matches();
+//        } else {
+//            return !username.trim().isEmpty();
+//        }
     }
 
     // A placeholder password validation check
     private boolean isPasswordValid(String password) {
-        return password != null && password.trim().length() > 5;
+        return true;
+        //return password != null && password.trim().length() > 5;
     }
 }
