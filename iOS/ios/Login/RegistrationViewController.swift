33596df7d6fa9d41d1af1a8baa293fa3d1f151e8
Dennis Fedorishin <dcfedori@buffalo.edu>
2019-11-09 22:50:34 -0500

    Signup endpoint call for iOS

--- iOS/ios/Login/RegistrationViewController.swift
+++ iOS/ios/Login/RegistrationViewController.swift
@@ -9,5 +9,20 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
+    @IBOutlet weak var FirstNameTextField: UITextField!
+    @IBOutlet weak var LastNameTextField: UITextField!
+    @IBOutlet weak var UserNameTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    
+    @IBAction func signup(_ sender: Any) {
+        api_signup(username: UserNameTextField.text ?? "", password: PasswordTextField.text ?? "", firstname: FirstNameTextField.text ?? "", lastname: LastNameTextField.text ?? "") { (success) in
+            if success {
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "performSignUpTransition", sender: self)
+                }
+            }
+        }
+    }
     
 }
